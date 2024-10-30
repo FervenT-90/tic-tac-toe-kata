635c3b7723e2039b9a62f574f110aa81e0c24cac
Milan <programming4live@gmail.com>
2024-10-30 19:18:02 +0000

    test(): Player X starts playing in a valid position

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,8 +1,21 @@
-import { Main } from "./main";
+import { TikTakToeGame } from "./main";
 
-describe("Default test", () => {
-  it("should work", () => {
-    const main = new Main("Hello world");
-    expect(main.getParam()).toBe("Hello world");
+describe("TickTackToe should", () => {
+
+  // empieza el jugador x
+  // debe elegir una posicion del 1 al 9
+  // la posicion elegida no debe estar cogida
+  // se cambia de turno cuando el jugador actual elige una posicion valida
+  // mientras esta jugando, el estatus es playing
+  // si el jugador x completa una fila, gana
+  // si el jugador o completa una columna, gana
+  // si el jugador x completa una diagonal, gana
+  // si no se cumple ninguna de las anteriores condiciones, quedan en empate
+  
+  it("Empieza el jugador x, eligiendo una posicion", () => {
+    const tickTackToeGame = new TikTakToeGame()
+    tickTackToeGame.play(5)
+    const result = "1 | 2 | 3\n4 | x | 6\n7 | 8 | 9"
+    expect(tickTackToeGame.getBoard()).toBe(result)
   });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,43 @@
-/**
- * Main Class
- */
-export class Main {
-  public static COMMANDS = 'Hello World';
+export class TikTakToeGame {
 
-  public param: string;
+  private board:string[][]= [
+    ["1","2","3"],
+    ["4","5","6"],
+    ["7","8","9"],
+  ]
+  
+  public play(position:number):void {
+    if (position < 1 || position > 9) {
+      throw new Error('Number must be between 1 and 9');
+    }
+    const row = Math.floor((position - 1) / 3);
+    const col = (position - 1) % 3;
 
-  constructor(param: string) {
-    this.param = param;
+    this.board[row][col] = "x"
   }
 
-  public getParam(): string {
-    return this.param;
+  public getBoard():string {
+    let msg = ""
+    this.board.forEach((row) => {
+      console.log({row})
+
+      row.forEach((field, index) => {
+        console.log({index})
+        if (index !== 0) {
+          msg += ` ${field} |`
+        } else {
+          msg += `${field} |`
+        }
+      })
+      msg = msg.slice(0, msg.length -2)
+      msg+= "\n"
+      
+    })
+    msg = msg.slice(0, msg.length -1)
+    return msg
+  }
+
+  public getStatus():string {
+    return ""
   }
 }
